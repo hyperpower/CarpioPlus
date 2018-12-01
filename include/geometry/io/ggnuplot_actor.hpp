f6c0a71b732cc724c9f5af2b518336d888aa244e
Hyper Power <hyper__power@hotmail.com>
2018-12-02 00:34:01 +0800

    plot revise

--- include/geometry/io/ggnuplot_actor.hpp
+++ include/geometry/io/ggnuplot_actor.hpp
@@ -58,19 +58,15 @@ public:
 			ITERPOINTS end,
 			int color_idx      =-1) {
 		ASSERT(Dim == 2);
+		int color = color_idx > 0? color_idx : 0;
 		spActor actor = spActor(new Gnuplot_actor());
 		actor->command() = "using 1:2:3 title \"\" ";
 		actor->style()   = "with points lc variable";
 
 		auto iterps = begin;
 		for (; iterps != end; iterps++) {
-			if (color_idx >= 0) {
-				actor->data().push_back(
-						ToString(iterps->x(), iterps->y(), color_idx, " "));
-			} else {
-				actor->data().push_back(
-						ToString(iterps->x(), iterps->y(), 0.0, " "));
-			}
+			actor->data().push_back(
+					ToString(iterps->x(), iterps->y(), color, " "));
 		}
 		actor->data().push_back("");
 		return actor;
@@ -149,23 +145,17 @@ public:
 			const Segment& seg,
 			int color_idx = -1) {
 		spActor actor = spActor(new Gnuplot_actor());
+		int color = color_idx > 0? color_idx : 0;
 		actor->command() = "using 1:2:3 title \"\" ";
 		actor->style()   = "with linespoints lc variable";
 		if (seg.empty()) {
 			actor->data().push_back("");
 			return actor;
 		}
-		if (color_idx >= 0) {
-			actor->data().push_back(ToString(seg.psx(), seg.psy(), color_idx, " "));
-		} else {
-			actor->data().push_back(ToString(seg.psx(), seg.psy(), 0, " "));
-		}
+		actor->data().push_back(ToString(seg.psx(), seg.psy(), color, " "));
+		actor->data().push_back(ToString(seg.pex(), seg.pey(), color, " "));
 
-		if (color_idx >= 0) {
-			actor->data().push_back(ToString(seg.pex(), seg.pey(), color_idx, " "));
-		} else {
-			actor->data().push_back(ToString(seg.pex(), seg.pey(), 0, " "));
-		}
+		actor->data().push_back("");
 		return actor;
 	}
 
@@ -173,6 +163,7 @@ public:
 			const PointChain& pc,
 			int color_idx = -1) {
 		spActor actor = spActor(new Gnuplot_actor());
+		int color = color_idx > 0? color_idx : 0;
 		actor->command() = "using 1:2:3 title \"\" ";
 		actor->style() = "with linespoints lc variable";
 		if (pc.empty()) {
@@ -181,19 +172,11 @@ public:
 		}
 
 		for (auto& p : pc) {
-			if (color_idx >= 0) {
-				actor->data().push_back(
-						ToString(p.x(), p.y(), color_idx, " "));
-			} else {
-				actor->data().push_back(ToString(p.x(), p.y(), 0, " "));
-			}
+			actor->data().push_back(
+					ToString(p.x(), p.y(), color, " "));
 		}
 
-		if (color_idx >= 0) {
-			actor->data().push_back(ToString(pc.front().x(), pc.front().y(), color_idx, " "));
-		} else {
-			actor->data().push_back(ToString(pc.front().x(), pc.front().y(), 0, " "));
-		}
+		actor->data().push_back(ToString(pc.front().x(), pc.front().y(), color, " "));
 		return actor;
 	}
 
@@ -201,6 +184,7 @@ public:
 			const PointChain& pc,
 			int color_idx = -1) {
 		spActor actor = spActor(new Gnuplot_actor());
+		int color = color_idx > 0? color_idx : 0;
 		actor->command() = "using 1:2:3:4:5 title \"\" ";
 		actor->style()   = "with vectors lc variable";
 		if (pc.empty()) {
@@ -212,215 +196,22 @@ public:
 		auto iterpe = pc.begin();
 		std::advance(iterpe, 1);
 		for (;iterpe !=pc.end();iterps++, iterpe++) {
-			if (color_idx >= 0) {
-				actor->data().push_back(
-						ToString(iterps->x(), iterps->y(),
-								 iterpe->x()-iterps->x() , iterpe->y()-iterps->y(), color_idx, " "));
-			} else {
-				actor->data().push_back(
-				        ToString(iterps->x(), iterps->y(),
-				        		 iterpe->x()-iterps->x() , iterpe->y()-iterps->y(), 0.0, " "));
-			}
+			actor->data().push_back(
+					ToString(iterps->x(), iterps->y(),
+							 iterpe->x()-iterps->x() ,
+							 iterpe->y()-iterps->y(), color, " "));
 		}
 		if (pc.closed()) {
-			if (color_idx >= 0) {
-				actor->data().push_back(
-						ToString(pc.back().x(), pc.back().y(),
-								pc.front().x() - pc.back().x(),
-								pc.front().y() - pc.back().y(), color_idx,
-								" "));
-			} else {
-				actor->data().push_back(
-						ToString(iterps->x(), iterps->y(),
-								iterpe->x() - iterps->x(),
-								iterpe->y() - iterps->y(), 0.0, " "));
-			}
+			actor->data().push_back(
+					ToString(pc.back().x(), pc.back().y(),
+			 				 pc.front().x() - pc.back().x(),
+							 pc.front().y() - pc.back().y(), color,
+							 " "));
 		}
 
 		return actor;
 	}
 };
-
-
-namespace GnuplotActor {
-
-
-template<typename TYPE>
-spActor LinesPoints(
-		const PointChain_<TYPE, 2>& pc,
-		int color_idx = -1) {
-	spActor actor = spActor(new Gnuplot_actor());
-	actor->command() = "using 1:2:3 title \"\" ";
-	actor->style() = "with linespoints lc variable";
-	if (pc.empty()) {
-		actor->data().push_back("");
-		return actor;
-	}
-
-	for (auto& p : pc) {
-		if (color_idx >= 0) {
-			actor->data().push_back(
-					ToString(p.x(), p.y(), color_idx, " "));
-		} else {
-			actor->data().push_back(ToString(p.x(), p.y(), 0, " "));
-		}
-	}
-
-	if (color_idx >= 0) {
-		actor->data().push_back(ToString(pc.front().x(), pc.front().y(), color_idx, " "));
-	} else {
-		actor->data().push_back(ToString(pc.front().x(), pc.front().y(), 0, " "));
-	}
-	return actor;
-}
-
-
-template<typename TYPE>
-spActor Arrows(
-		const PointChain_<TYPE, 2>& pc,
-		int color_idx = -1) {
-	spActor actor = spActor(new Gnuplot_actor());
-	actor->command() = "using 1:2:3:4:5 title \"\" ";
-	actor->style() = "with vectors lc variable";
-	if (pc.empty()) {
-		actor->data().push_back("");
-		return actor;
-	}
-
-	auto iterps = pc.begin();
-	auto iterpe = pc.begin();
-	std::advance(iterpe, 1);
-	for (;iterpe !=pc.end();iterps++, iterpe++) {
-		if (color_idx >= 0) {
-			actor->data().push_back(
-					ToString(iterps->x(), iterps->y(),
-							 iterpe->x()-iterps->x() , iterpe->y()-iterps->y(), color_idx, " "));
-		} else {
-			actor->data().push_back(
-			        ToString(iterps->x(), iterps->y(),
-			        		 iterpe->x()-iterps->x() , iterpe->y()-iterps->y(), 0.0, " "));
-		}
-	}
-
-	if (color_idx >= 0) {
-		actor->data().push_back(
-				ToString(pc.back().x(),
-						 pc.back().y(),
-						 pc.front().x() - pc.back().x(),
-						 pc.front().y() - pc.back().y(), color_idx, " "));
-	} else {
-		actor->data().push_back(
-				ToString(iterps->x(), iterps->y(), iterpe->x() - iterps->x(),
-						iterpe->y() - iterps->y(), 0.0, " "));
-	}
-
-	return actor;
-}
-
-
-
-}
-
-//template<typename TYPE, St DIM>
-//class GPA_Geometry_ {
-//public:
-//	static const St Dim = DIM;
-//	typedef TYPE Vt;
-//	typedef Point_<TYPE, DIM> Point;
-//	typedef Point_<TYPE, DIM>& ref_Point;
-//	typedef const Point_<TYPE, DIM>& const_ref_Point;
-//	typedef Segment_<TYPE, DIM> Segment;
-//	typedef Segment& ref_Segment;
-//	typedef const Segment& const_ref_Segment;
-//
-//	typedef Contour_<TYPE> Contour;
-//	typedef PointChain_<TYPE, DIM> PointChain;
-//	typedef Polygon_<TYPE> Polygon;
-//
-//	typedef std::shared_ptr<Gnuplot_actor> spActor;
-//	typedef std::list<spActor> list_spActor;
-//
-//public:
-//	static spActor Lines(const Contour& contour, int base_idx = 0,
-//			int color_idx = -1) {
-//		spActor actor = spActor(new Gnuplot_actor());
-//		actor->command() = "using 1:2:3 title \"\" ";
-//		actor->style() = "with lines lc variable";
-//		if (contour.empty()) {
-//			actor->data().push_back("");
-//			return actor;
-//		}
-//		for (St i = 0; i < contour.size_vertices(); ++i) {
-//			const Point& p = contour.v(i);
-//			if (color_idx >= 0) {
-//				actor->data().push_back(ToString(p.x(), p.y(), color_idx, " "));
-//			} else {
-//				actor->data().push_back(
-//						ToString(p.x(), p.y(), i + base_idx, " "));
-//			}
-//		}
-//		const Point& pstart = contour.v(0);
-//		if (color_idx >= 0) {
-//			actor->data().push_back(
-//					ToString(pstart.x(), pstart.y(), color_idx, " "));
-//		} else {
-//			actor->data().push_back(
-//					ToString(pstart.x(), pstart.y(), base_idx, " "));
-//		}
-//		actor->data().push_back("");
-//		return actor;
-//	}
-//
-//	static spActor Lines(const PointChain& points, int base_idx = 0,
-//			int color_idx = -1) {
-//		spActor actor = spActor(new Gnuplot_actor());
-//		actor->command() = "using 1:2:3 title \"\" ";
-//		actor->style() = "with lines lc variable";
-//		if (points.empty()) {
-//			actor->data().push_back("");
-//			return actor;
-//		}
-//		int i = 0;
-//		for (auto& p : points) {
-//			if (color_idx >= 0) {
-//				actor->data().push_back(ToString(p.x(), p.y(), color_idx, " "));
-//			} else {
-//				actor->data().push_back(
-//						ToString(p.x(), p.y(), i + base_idx, " "));
-//			}
-//			i++;
-//		}
-//		// If point chain is closed, the first point should be add.
-//		if (points.closed()) {
-//			const Point& pstart = *(points.begin());
-//			if (color_idx >= 0) {
-//				actor->data().push_back(
-//						ToString(pstart.x(), pstart.y(), color_idx, " "));
-//			} else {
-//				actor->data().push_back(
-//						ToString(pstart.x(), pstart.y(), base_idx, " "));
-//			}
-//		}
-//		actor->data().push_back("");
-//		return actor;
-//	}
-//
-//	static spActor Points(const Point& p, int color_idx = -1) {
-//		spActor actor = spActor(new Gnuplot_actor());
-//		actor->command() = "using 1:2:3 title \"\" ";
-//		actor->style() = "with points lc variable";
-//
-//		if (color_idx >= 0) {
-//			actor->data().push_back(ToString(p.x(), p.y(), color_idx, " "));
-//		} else {
-//			actor->data().push_back(ToString(p.x(), p.y(), 0, " "));
-//		}
-//
-//		actor->data().push_back("");
-//		return actor;
-//	}
-//
-//};
 
 }
 
