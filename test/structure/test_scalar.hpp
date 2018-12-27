8cd30d9524a1f0e19cee9a4e307101cf323393b9
Hyper Power <hyper__power@hotmail.com>
2018-12-28 01:28:42 +0800

    nothing

--- test/structure/test_scalar.hpp
+++ test/structure/test_scalar.hpp
@@ -217,7 +217,38 @@ TEST(scalar, DISABLED_add){
 
 
 TEST(scalar, plot_vector_face){
+	std::cout << "field_define 3d\n";
+	const St Dim = 1;
+	typedef SField_<Dim>                        Field;
+	typedef std::shared_ptr<SGrid_<Dim> >     spGrid;
+	typedef std::shared_ptr<SGhost_<Dim> >    spGhost;
+	typedef std::shared_ptr<SOrderXYZ_<Dim> > spOrder;
+	typedef Point_<Vt, Dim> Point;
+	Point pmin(0, 0, 0);
+	Point pmax(1, 1, 1);
+	spGrid  spsg(new SGridUniform_<Dim>(pmin, { 5, 5, 5}, 0.3, 2));
 
+	spGhost spgh(new SGhostRegular_<Dim>(spsg));
+
+	spOrder  sporder(new SOrderXYZ_<Dim>(spsg, spgh));
+
+	Field sc(spsg, spgh, sporder);
+	sc.assign([](Vt x, Vt y, Vt z, Vt t){
+		return sin(x);
+	});
+
+	typedef SGnuplotActor_<Dim> GA;
+	Gnuplot gnu;
+	gnu.set_terminal_png("3d.png");
+	gnu.set_xrange(-0.1, 1.6);
+	gnu.set_yrange(-0.1, 1.6);
+	gnu.set_zrange(-0.1, 1.6);
+	auto awf = GA::WireFrame(*spsg);
+	gnu.add(awf);
+	//	awf->show_data();
+	gnu.plot();
+
+	//	SIOFile_<1>::OutputField("1d.txt", sc);
 
 }
 
